f6a7892d1bc0a530cb0dd00f1c08ee9dc72b367e
licetullmann <licetrosales@gmail.com>
2022-10-25 15:32:03 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-        int a;
+        int a,b;
     System.out.println("sdf sdfsdf gfdg fancy");
+
 
 }
