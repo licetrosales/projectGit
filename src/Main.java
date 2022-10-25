8df8f4d777c95808f464c979c8b9a4e582800004
licetullmann <licetrosales@gmail.com>
2022-10-25 16:12:52 +0200

    fifth commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int a=9;
+        int b=5;
         System.out.println("sdf sdfsdf gfdg fancy");
+
     }
 
 
